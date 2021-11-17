8d40ea0783215acc448512a818f6d86d1a582a7d
zhangchun <491812945@qq.com>
2021-11-17 15:59:07 +0800

    2021年11月17日

---
+++ src/pers/jojo/leetcode/problems/BulbSwitch.java
@@ -0,0 +1,14 @@
+package pers.jojo.leetcode.problems;
+
+/**
+ * @ClassName BulbSwitch
+ * @Description https://leetcode-cn.com/problems/bulb-switcher/
+ * @Author 张淳
+ * @Date 2021/11/15 09:24
+ * @Version 1.0.0
+ **/
+public class BulbSwitch {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
