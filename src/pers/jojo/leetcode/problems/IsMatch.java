427f804b45c14712e10e02f50e1e5d97329057e3
zhangchun <491812945@qq.com>
2021-12-21 14:57:05 +0800

    2021年12月21日

---
+++ src/pers/jojo/leetcode/problems/IsMatch.java
@@ -0,0 +1,14 @@
+package pers.jojo.leetcode.problems;
+
+/**
+ * @ClassName IsMatch
+ * @Description https://leetcode-cn.com/problems/regular-expression-matching/
+ * @Author 张淳
+ * @Date 2021/12/15 15:10
+ * @Version 1.0.0
+ **/
+public class IsMatch {
+    public boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+}
